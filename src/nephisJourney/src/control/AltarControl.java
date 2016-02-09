8dc38c6d23af800f80713eac634274696c6e1e3b
Hodes <Hodes@Hodes-PC.nv.charter.com>
2016-02-08 22:44:38 -0800

    Updated the AltarControl class, changed the algorithm to a function. Stopped at Step 6 of Lesson 5 - team assignment.

--- src/nephisJourney/src/control/AltarControl.java
+++ src/nephisJourney/src/control/AltarControl.java
@@ -10,5 +10,24 @@ package nephisJourney.src.control;
  * @author Jenaca
  */
 public class AltarControl {
+
+//modifiers returnType functionName (datatype paramerer1, ...)
+    public double calcAltarSize(double height, double width) { 
+        if (height < 1 || height > 4){ //test to check if the height is in range
+		return -1;
+        }
+	if (width < 1 || width > 4) { //test to check if the width is in range
+		return -2;
+        }
+        
+        double radius = width / 2; //divides the width to give the radius
+        int volume =(int) (Math.PI * Math.pow(radius, 2) * height);
+        /*finds the volume of the Altar and return it as a whole number of 
+        stones needed
+        */ 
+        return volume;
+
+    }
+
     
 }
