a9805d80339d267c792dd69b37cda2b3b273ca10
ahalve01 <adamhalverson@gmail.com>
2016-02-12 22:46:47 -0800

    Lesson5 Individual Assignment HuntingControl Completed

--- src/nephisJourney/src/control/HuntingControl.java
+++ src/nephisJourney/src/control/HuntingControl.java
@@ -7,8 +7,33 @@ package nephisJourney.src.control;
 
 /**
  *
- * @author Jenaca
+ * @author Adam
  */
 public class HuntingControl {
     
+    
+    
+    public static void main(String[] args){
+        
+        String Hunt;
+        int random = (int ) (Math.random() * 135 + 1);
+        
+        
+        if (random >=5 && random <=15){
+            Hunt = "You hunted a Hyrax that weighed "+ random + "lbs!";
+        } 
+        else if (random >=40 && random <=80){
+            Hunt = "You hunted a Gray Wolf that weighed "+ random + "lbs!";
+        }
+        else if (random >=100 && random <=120){
+            Hunt = "You hunted a Arabian Oryx that weighed "+ random + "lbs!";
+        }
+        else{
+            Hunt = "Hunt failed!";
+        }
+        System.out.println(Hunt);
+    }    
+
+
+         
 }
---
+++ test/nephisJourney/src/control/HuntingControlTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nephisJourney.src.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author adamh_000
+ */
+public class HuntingControlTest {
+    
+    public HuntingControlTest() {
+    }
+
+    /**
+     * Test of main method, of class HuntingControl.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        HuntingControl.main(args);
+     
+    }
+    
+}
